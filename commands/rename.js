95763d8253149db7aec66104b1de439c7c309ec1
Alexis Massa <>
2021-04-28 23:13:05 +0000

    changed to reaction → send

--- commands/rename.js
+++ commands/rename.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: 'rename',
-    description: 'Rename from name1 to name2',
-    usage: '-rename',
-    accessableby: 'Members',
-    aliases: ['rn'],
-    execute(message, args) {
-        message.channel.send(`renamed ${args[0]} into ${args[1] + ' ' + args[2]}`)
-            .then(message.react(':✅:'))
-            .then(message.react('❌'));
-    }
+  name: 'rename',
+  description: 'Rename from name1 to name2',
+  usage: '-rename',
+  accessableby: 'Members',
+  aliases: ['rn'],
+  execute(message, args) {
+    message.react('\u2705')
+      .then(message.react('\u274c')).then(
+        message.channel.send(`renamed ${args[0]} into ${args[1] + ' ' + args[2]}`));
+  }
 };
