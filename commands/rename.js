4f96c01d53996db5d602ca85cb638905ec478227
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2021-04-29 03:16:17 +0200

    continued

--- commands/rename.js
+++ commands/rename.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
   name: 'rename',
   description: 'Rename from name1 to name2',
@@ -5,8 +7,27 @@ module.exports = {
   accessableby: 'Members',
   aliases: ['rn'],
   execute(message, args) {
-    message.react('\u2705')
-      .then(message.react('\u274c')).then(
-        message.channel.send(`renamed ${args[0]} into ${args[1] + ' ' + args[2]}`));
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Rename')
+      .setColor('#5900ff')
+      .setDescription('Rename demand.')
+      // TODO : add info fields here
+      .addFields([
+        { name: 'from', value: 'value', inline: true },
+        { name: 'to', value: 'value', inline: true },
+        { name: 'status', value: 'wait', inline: true }
+      ])
+      .setTimestamp();
+
+    message.channel.send({ embed })
+      .then(newMsg => {
+        newMsg.react('\u2705');
+        newMsg.react('\u274c');
+        newMsg.react('🚫');
+      }).then(message.delete({ reason: 'deleted by bot' }));
+
+    // message.channel.send(`${message.author.tag} rename ${args[0]} to ${args[1] + ' ' + args[2]} (waiting for confirmation)`);
+
   }
 };
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,45 @@
 module.exports = {
-    name: 'reaction',
-    execute(reaction, user) {
-        console.log(`${user} in #${reaction.message.channel.name} reacted with : ${reaction}`);
+    name: 'messageReactionAdd',
+    execute: async (reaction, user) => {
+
+        // try to get reaction
+        try {
+            await reaction.fetch();
+        } catch (e) {
+            console.log(`event : messageReactionAdd - error : ${e}`);
+            return;
+        }
+
+        console.log(`${user} in #${reaction.message.channel.name} reacted with : ${reaction.emoji.name}`);
+        reaction.message.guild.members.fetch(user.id).then(member => {
+            if (!user.id === reaction.message.author.id && member.hasPermission('ADMINISTRATOR')) {
+                if (reaction.emoji.name == '\u2705') {
+                    validate();
+                }
+                if (reaction.emoji.name == '\u274c') {
+                    refuse();
+                }
+            } else if (user.id === reaction.message.author.id && reaction.emoji.name == '\u274c') {
+                cancel();
+            } else {
+                console.log('not admin reacted');
+            }
+        });
     }
 };
+
+// TODO : execute rename
+function validate() {
+    //
+    console.log('ok');
+}
+// TODO : something wrong
+function refuse() {
+    //
+    console.log('not ok');
+}
+
+function cancel() {
+    //
+    console.log('cancel');
+}
