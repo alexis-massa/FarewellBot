a8823be0d91fb72be46eccd728f69dcabd507734
Alexis Massa <>
2021-04-30 17:22:37 +0000

    ok

--- database.js
+++ database.js
@@ -4,36 +4,38 @@ const Database = require('@replit/database');
 // database
 const db = new Database();
 
-// add default value for test
-const setup = function () {
-    db.set('key', 'value');
-    db.get('key').then(value => { console.log(value); }).catch(e => { console.log(e); });
-};
-
 // get all values in db
 const getAllValues = async function () {
     const values = db.getAll();
     return values;
 };
 
+const empty = function () {
+    db.list().then(keys=>{
+      for(let i; i > keys.length; i++){
+        db.delete(keys[i]);
+      }
+    });
+};
+
 const add = async function (newKey, newValue) {
 
-    db.list().then(keys => {
-        keys.foreach(key => {
-            db.delete(key);
-        });
-    });
 
-    db.get(newKey).then(value => {
+    await db.get(newKey).then(value => {
+      console.log(value);
         if (value) {
             console.log('exists');
+            db.set(newKey, newValue);
         } else {
             console.log('not exist');
+            db.set(newKey, newValue);
         }
     });
-
+        await db.list().then(keys => {
+            console.log(keys);
+        });
     // db.set(key, value);
 };
 
 
-module.exports = { setup, getAllValues, add };
+module.exports = { getAllValues, add };
