fc77a5cd776b88547b91739306341582fdf0dd5e
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2021-10-01 08:39:42 +0000

    welcome message OK

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,12 @@
 module.exports = {
-    name: 'guildMemberAdd',
-    /**
-     * @param { Discord.client } client
-     */
-    execute(member) {
-        member.guild.channels.get("789762485258813445").send(`Welcome <@${member.id}>, great to have you! Feel free to introduce yourself and check out #roles to get started.`);
-    }
+  name: 'guildMemberAdd',
+  /**
+   * @param { Discord.client } client
+   */
+  execute(member, client) {
+
+    client.channels.fetch("789762485053685761").then(channel => {
+      channel.send(`Welcome <@${member.id}>, great to have you! Feel free to introduce yourself ${client.channels.cache.get('789762484776337436').toString()}  and then check out ${client.channels.cache.get('838393771259461652').toString()} to get started.`);
+    });
+  }
 };
