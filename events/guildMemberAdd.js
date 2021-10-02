d92bf1fbac70d55711df140cd1627fcae1da895e
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2021-10-02 14:54:55 +0000

    updated join message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,7 +6,7 @@ module.exports = {
   execute(member, client) {
 
     client.channels.fetch("789762485053685761").then(channel => {
-      channel.send(`Welcome <@${member.id}>, great to have you! Feel free to introduce yourself ${client.channels.cache.get('789762484776337436').toString()}  and then check out ${client.channels.cache.get('838393771259461652').toString()} to get started.`);
+      channel.send(`Welcome <@${member.id}>! Feel free to introduce yourself, check ${client.channels.cache.get('789762484776337436').toString()} for the server rules and overview,  and then head to ${client.channels.cache.get('838393771259461652').toString()} to get started.`);
     });
   }
 };
